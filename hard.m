7081e9dace68d2f837ccbab816f17fb607469a7c
Victor Chen <chen116@usc.edu>
2017-06-21 05:44:02 -0700

    Update hard.m

--- hard.m
+++ hard.m
@@ -23,8 +23,8 @@ while not_fea
   B = randi([8 64],m,m);
   B(eye(size(B))~=0)=10e5;
   f =  randi([18 24],1,m)*150;%(MHz)
-  N=  randi([6 10],1,m)*3; % number of tasks(cars)
-  C =  randi([6 10],1,m)*3; % server link capacity (# of tasks)
+  N=  randi([4 8],1,m)*3; % number of tasks(cars)
+  C =  randi([4 8],1,m)*3; % server link capacity (# of tasks)
   
   num_tasks_allowed = sum(floor(tau*f/x));
   num_tasks = sum(N);
@@ -48,7 +48,7 @@ for iter = 1:5
 
      not_fea = 1;
      while not_fea 
-       N=  randi([6 10],1,m)*3; % number of tasks(cars)
+       N=  randi([4 8],1,m)*3; % number of tasks(cars)
        num_tasks_allowed = sum(floor(tau*f/x));
        num_tasks = sum(N);
        if num_tasks < num_tasks_allowed & num_tasks < sum(C)
