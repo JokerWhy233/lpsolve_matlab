5b1aaaa0576e3b8fc307d52bfcf929e39d093637
Victor Chen <yvictorck@gmail.com>
2017-07-17 04:24:43 -0700

    a

--- try.sh
+++ try.sh
@@ -4,4 +4,4 @@ out=$3
 para=$4
 
 #git pull && qsub h3.pbs -N $NAME -v matname="$MATFILE",outtxt="$OUT",x="$PARA"
-git pull && qsub h3.pbs -N $name -v matname="$matfile",outtxt="$out",m="$para"
+git pull && qsub h3.pbs -N no$name -v matname="no$matfile",outtxt="no$out",m="$para" && qsub h3.pbs -N $name -v matname="$matfile",outtxt="$out",m="$para"
