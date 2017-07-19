72df62e81258d20e60cc9460b5fabe3b774684ff
Victor Chen <yvictorck@gmail.com>
2017-07-19 06:00:20 -0700

    a

--- try.sh
+++ try.sh
@@ -14,5 +14,5 @@ noname=no$fullname
 nomatfile=no$matfile
 nooutfile=no$fullname$out
 
-qsub h3.pbs -N $noname -v matname="$nomatfile",outtxt="$nooutfile",m="$m",A="$A"
-qsub h3.pbs -N $fullname -v matname="$matfile",outtxt="$outfile",m="$m",A="$A"
+qsub h3.pbs -N $noname -v matname="$nomatfile",outtxt="$nooutfile",m="$m",A="$a"
+qsub h3.pbs -N $fullname -v matname="$matfile",outtxt="$outfile",m="$m",A="$a"
