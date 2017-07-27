7159ff8db42dde10eea8a60510bf5f0441f1ac2c
Victor Chen <yvictorck@gmail.com>
2017-07-27 14:00:37 -0700

    a

--- run-m.sh
+++ run-m.sh
@@ -1,4 +1,3 @@
 source /usr/usc/matlab/default/setup.sh
 export LD_LIBRARY_PATH="/home/rcf-40/chen116/dynamo/lpsolve_matlab"
-screen -S vc -d -m && screen -r vc -X stuff $' source /usr/usc/matlab/default/setup.sh \n export LD_LIBRARY_PATH="/home/rcf-40/chen116/dynamo/lpsolve_matlab" \n matlab -nodesktop \n'
-
+screen -S vc -d -m && screen -r vc -X stuff $' source /usr/usc/matlab/default/setup.sh \n export LD_LIBRARY_PATH="/home/rcf-40/chen116/dynamo/lpsolve_matlab" \n matlab -nodesktop \n' && screen -r
