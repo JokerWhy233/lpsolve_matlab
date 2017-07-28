ac1d8770fc59d5e7e3f857e26ddcbd2aca8a5679
Victor Chen <yvictorck@gmail.com>
2017-07-27 17:40:34 -0700

    a

--- demo.m
+++ demo.m
@@ -108,7 +108,7 @@ for iter = 1:total_iter
 %     end
     for i=1:m
         for j=1:m
-            obj=[obj x/f(j)*(A*(f(j)/150)^p+E)]; 
+            obj=[obj x/f(j)*(A*(f(j))^p+E)]; 
         end
     end
     for i=1:m
