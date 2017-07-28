d77fc2cf9dc5ee44c0bd7a8abd5a98dffa60e867
Victor Chen <yvictorck@gmail.com>
2017-07-28 07:16:49 -0700

    a

--- demo.m
+++ demo.m
@@ -60,6 +60,7 @@ not_fea = 1;
   f =  randi([18 24],1,m)*150;%(MHz)
   f(end)=30*150;
   N=  randi([new_t new_t+10],1,m); % number of tasks(cars)
+  N(end)=0;
   C =  randi([new_t new_t+10],1,m); % server link capacity (# of tasks)
   C(end)=inf;
   num_tasks_allowed = sum(floor(tau*f/x));
@@ -88,6 +89,7 @@ for iter = 1:total_iter
      not_fea = 1;
 %      while not_fea 
        N=  randi([new_t new_t+20],1,m); % number of tasks(cars)
+       N(end)=0;
        num_tasks_allowed = sum(floor(tau*f/x));
        num_tasks = sum(N);
        if num_tasks < num_tasks_allowed & num_tasks < sum(C)
