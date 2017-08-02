f3e31956f9ad3c1b3a54e4b12cf94105dee73527
Victor Chen <yvictorck@gmail.com>
2017-08-01 19:50:10 -0700

    a

--- rb.m
+++ rb.m
@@ -16,7 +16,7 @@ rng('default');
 % v=10;
 
 Deadline = 0.5;% deadline (sec)
-tau = 0.48; % allocated cpu time (sec)
+tau = 0.5; % allocated cpu time (sec)
 x= 50; % Mcycles per task
 %v=10; 
 E=70;
@@ -55,7 +55,7 @@ for j = 1:length(N_mat)
         time_passed = 0;
         for n=1:N(i)
             time_passed = time_passed +  1/B(i,i) + x/f(i);      
-            if  time_passed <= Deadline || n <= C(i)
+            if  time_passed <= Deadline && n <= C(i)
                 N_leftover(i)=N_leftover(i)+1;
                 static_enegery_used=static_enegery_used+(A*(f(i)/1000)^p+E)+x/f(i);  
                 
@@ -106,7 +106,7 @@ rng('default');
 % v=10;
 
 Deadline = 0.5;% deadline (sec)
-tau = 0.48; % allocated cpu time (sec)
+tau = 0.5; % allocated cpu time (sec)
 x= 50; % Mcycles per task
 %v=10; 
 E=70;
@@ -140,41 +140,74 @@ for j = 1:length(N_mat)
 
     N = N_mat(j,:);
     N_leftover=zeros(1,size(N_mat,2));
+    C_left = C;
 
+    time_passed_ar = zeros(1,size(N_mat,2));
     for i=1:m
-        time_passed = 0;
+        time_passed = time_passed_ar(i);
         for n=1:N(i)
             time_passed = time_passed +  1/B(i,i) + x/f(i);      
-            if  time_passed <= Deadline || n <= C(i)
+            if  time_passed <= Deadline && n <= C(i)
                 N_leftover(i)=N_leftover(i)+1;
                 static_enegery_used=static_enegery_used+(A*(f(i)/1000)^p+E)+x/f(i);  
+                C_left(i)=C_left(i)-1;
+            
                 
-            else
-                N(i+1)=N(i+1)+1;
             end
             
         end
+        time_passed_ar(i)=time_passed;
         N_leftover(i)=N(i)-N_leftover(i);
 
     end
-    time_passed = 0;
-
-    for n=1:m
-            
-            time_passed = time_passed +  1/4 + x/f(end);   
-            
-            if  time_passed >= Deadline
-                miss_cnt = miss_cnt + 1;
-            else
-                no_miss_cnt=no_miss_cnt+1;
-            end   
+    inn=0;
+    N_leftover
+    time_passed_ar
+    for i=1:m
+        
+        while(N_leftover(i)>0)
+            for ii=1:m
+%                 time_passed = Deadline-time_passed_ar(ii);
+%                
+                if C_left(ii)>0
+                    time_passed_ar(ii) = time_passed_ar(ii) +1/B(i,ii)+ x/f(ii);
+                    if time_passed_ar(ii)>Deadline
+                        miss_cnt = miss_cnt + 1;
+                    end
+                    static_enegery_used=static_enegery_used+(A*(f(ii)/1000)^p+E)+x/f(ii);  
+                    N_leftover(i)=N_leftover(i)-1;
+                    C_left(ii)=C_left(ii)-1;
+       
+                         inn=inn+ii;
+                    
+                end
+            end
+        end
+        
+        
+        
     end
     
     
-      
-     static_enegery_used=static_enegery_used+  (A*(f(end)/1000)^p+E)*sum(N_leftover)*x/f(end);  
-         
-        
+    
+%     time_passed = 0;
+% 
+%     for n=1:m
+%             
+%             time_passed = time_passed +   1/B(1,end) + x/f(end);   
+%             
+%             if  time_passed >= Deadline
+%                 miss_cnt = miss_cnt + 1;
+%             else
+%                 no_miss_cnt=no_miss_cnt+1;
+%             end   
+%     end
+%     
+%     
+%       
+%      static_enegery_used=static_enegery_used+  (A*(f(end)/1000)^p+E)*sum(N_leftover)*x/f(end);  
+%          
+%         
 end
 miss_cnt
 no_miss_cnt
